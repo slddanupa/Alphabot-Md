bb26493641f734469d9a0397959c178f2a83eea7
slddanupa <127113585+slddanupa@users.noreply.github.com>
2023-03-24 05:08:26 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,17 +28,17 @@ global.language = english  //change indonesia to english if you don't understand
 // Other
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ RASHMIKA ×፝֟͜×" //nama kalian
+global.ownername= "ᴹᴿ᭄ DANUPA ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
 global.email = "janith20062006@gmail.com" //bebas
 global.region = "Sri Lanka" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['94717775628'] //premium user
-global.owner = ["94717775628"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "94717775628" // nomor wa kalian
-global.ownernomerr = "+94717775628" //nmr wa kalian
+global.premium = ['94774500937'] //premium user
+global.owner = ["94774500937"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "94774500937" // nomor wa kalian
+global.ownernomerr = "+94774500937" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
